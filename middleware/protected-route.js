e7cf581e50dc0a0af2cd2e7b0868bb571dd873a4
Joel Bartlett <joel@murbar.com>
2019-04-03 17:29:16 -0400

    Add middleware

---
+++ middleware/protected-route.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+const { jwtSecret } = require('../config');
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization;
+    const decodedToken = jwt.verify(token, jwtSecret);
+    req.decodedJwt = decodedToken;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ message: 'Invalid Credentials.' });
+  }
+};
